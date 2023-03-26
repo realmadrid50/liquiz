70cccbc4a8b0a0252133b9557b730c36fa744030
theghostyousee <patrickserise174@gmail.com>
2023-03-26 13:39:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import './index.css';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  function clicktweet() {
+    const url = "https://twitter.com/intent/tweet?text=This%20tweet%20is%20my%20registration%20proof%20for%20%40Osmo_Dex%20your%20first%20%20protocol%20for%20trading%20built%20on%20%23ZKsync.%0D%0A%0D%0AI%27m%20ready%20to%20get%20my%20early%20access.&original_referer=https://clicktotweet.com&related=clicktotweet";
+    window.open(url);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <div className='app-wrapper'>
+        <img src={logo}></img>
+        <h1>OSMO</h1>
+        <div className='paragraph'>
+          <p>Osmo's mission is to unlock<br/> humankind’s true potential in ever-<br/>inclusive realities</p>
+        </div>
+        <button onClick={clicktweet}>PROOF OF PARTICIPATION</button>
+        
+      </div>
     </div>
   );
 }
