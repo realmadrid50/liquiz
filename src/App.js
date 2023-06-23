4e977d9af521b0edc98bd263db7dd1a56ad56f3a
theghostyousee <patrickserise174@gmail.com>
2023-06-23 13:09:56 +0200

    s

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './index.css';
-import logo from './logo.svg';
+import logo from './logo.png';
 
 function App() {
 
   function clicktweet() {
-    const url = "https://twitter.com/intent/tweet?text=This%20tweet%20is%20my%20registration%20proof%20for%20%40Osmo_Dex%20your%20first%20%20protocol%20for%20trading%20built%20on%20%23ZKsync.%0D%0A%0D%0AI%27m%20ready%20to%20get%20my%20early%20access.&original_referer=https://clicktotweet.com&related=clicktotweet";
+    const url = "https://twitter.com/intent/tweet?text=This%20tweet%20is%20my%20registration%20proof%20for%20%40CraxFinance%20your%20Yield%20Aggregator%20built%20on%20%40ethereum.%0D%0A&";
     window.open(url);
   }
   
@@ -13,10 +13,9 @@ function App() {
 
 
       <div className='app-wrapper'>
-        <img src={logo}></img>
-        <h1>OSMO</h1>
+        <h1>Crax</h1>
         <div className='paragraph'>
-          <p>Osmo's mission is to unlock<br/> humankind’s true potential in ever-<br/>inclusive realities</p>
+          <p>Crax's unlocks real<br/> Yield Aggregator on Ethereum<br/></p>
         </div>
         <button onClick={clicktweet}>PROOF OF PARTICIPATION</button>
         
