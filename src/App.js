aa7cc244d20c2d0fc61955e4b107c2f29214beca
realmadrid50 <wozals77@gmail.com>
2023-08-12 14:34:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './index.css';
-import logo from './logo.png';
-import Discord from './icon.svg'
+import Discord from './t.png'
 
 function App() {
 
   function clicktweet() {
-    const url = "https://twitter.com/intent/tweet?text=This%20tweet%20is%20my%20registration%20proof%20for%20%40CraxFinance%20your%20Yield%20Aggregator%20built%20on%20%40ethereum.%0D%0A&";
+    const url = "https://twitter.com/intent/tweet?text=This%20tweet%20is%20my%20registration%20proof%20for%20%40LiquizFinance%20%F0%9F%A6%8B%0D%0A%0D%0AThe%20Center%20of%20DeFi%20built%20on%20%40BuildOnBase&";
     window.open(url);
   }
   
   function discord() {
-    const url = "https://discord.com/invite/EP44K4KJ7A";
+    const url = "https://t.me/LiquizFinance";
     window.open(url);
   }
 
@@ -19,10 +18,7 @@ function App() {
 
 
       <div className='app-wrapper'>
-        <h1>Crax</h1>
-        <div className='paragraph'>
-          <p>Crax's unlocks real<br/> Yield Aggregator on Ethereum<br/></p>
-        </div>
+        <h1>Liquiz COMING <br/>ON BASE </h1>
         <button onClick={clicktweet}>PROOF OF PARTICIPATION</button>
 
 
